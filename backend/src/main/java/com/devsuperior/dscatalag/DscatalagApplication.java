2e2d3cc565e821df16c2df11bf3e718fcdff8e5a
Mauricio Vaz <vazmau@gmail.com>
2020-09-30 22:44:44 -0400

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscatalag/DscatalagApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalagApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalag/DscatalagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
