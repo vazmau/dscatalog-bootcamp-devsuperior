e624b7ee7d0821081ea65c1cb3a5190a26dbf1e7
Mauricio Vaz <vazmau@gmail.com>
2020-10-20 00:07:06 -0400

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalag/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalag/entities/Category.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dscatalag.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 
@@ -18,7 +22,12 @@ public class Category  implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updateAt;
 	
 	public Category() {
 	}
@@ -44,6 +53,27 @@ public class Category  implements Serializable {
 		this.name = name;
 	}
 
+	
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdateAt() {
+		return updateAt;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updateAt = Instant.now();
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
